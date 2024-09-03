cc48c738339fc17031b4634a4fd3ad03e6545dd6
Maneesha-Rupasinghe <maneelakshan@gmail.com>
2024-09-03 09:34:42 +0530

    data analytic page phase 1 complete

--- src/components/data_analytics/sub_section_3/Section3Block.tsx
+++ src/components/data_analytics/sub_section_3/Section3Block.tsx
@@ -1,16 +1,32 @@
 import React from 'react'
 import Section3Data from './Section3Data'
+import { title } from 'process'
 
 type Section3BlockProps = {
   reverse: boolean
+  button1Name: string,
+  button2Name: string,
+  button3Name: string,
+  button4Name: string,
+  button5Name: string,
+  tittle: string,
+  content: string,
 }
-function Section3Block({ reverse }: Section3BlockProps
+function Section3Block({ reverse, button1Name, button2Name, button3Name, button4Name, button5Name, tittle, content }: Section3BlockProps
 ) {
   return (
     <div className={`w-full h-full flex ${reverse ? 'flex-row-reverse' : 'flex-row'}`}>
-      <div className='w-[55%] h-full bg-gray-400 flex justify-end items-center py-[3.77%] '>
-        <div className={`w-full h-full  bg-red-500  ${reverse ? 'ml-[7.54%]' : 'mr-[7.54%]'} `}>
-          <Section3Data />
+      <div className='w-[55%] h-full  flex justify-end items-center py-[3.77%] '>
+        <div className={`w-full h-full    ${reverse ? 'ml-[7.54%]' : 'mr-[7.54%]'} `}>
+          <Section3Data
+            button1Name={button1Name}
+            button2Name={button2Name}
+            button3Name={button3Name}
+            button4Name={button4Name}
+            button5Name={button5Name}
+            title={tittle}
+            content={content}
+          />
         </div>
 
       </div>
--- src/components/data_analytics/sub_section_3/Section3Data.tsx
+++ src/components/data_analytics/sub_section_3/Section3Data.tsx
@@ -1,22 +1,39 @@
 import React from 'react';
 import ButtonSection from './ButtonSection';
 
-function Section3Data() {
+type ButtonSectionProps = {
+    button1Name: string,
+    button2Name: string,
+    button3Name: string,
+    button4Name: string,
+    button5Name: string,
+    title: string,
+    content: string,
+
+}
+function Section3Data({ button1Name, button2Name, button3Name, button4Name, button5Name, title, content }: ButtonSectionProps) {
     return (
         <div className='w-full h-full flex flex-col justify-between'>
             {/* First div - Top */}
-            <div className="flex-shrink-0 text-[40px] leading-[47.73px] font-[600px]">
-                Actionable Insights for Informed Decision Making
+            <div className="flex-shrink-0 text-[40px] leading-[47.73px] font-[600px] text-black">
+                {title}
             </div>
 
             {/* Second div - Middle */}
             <div className="flex-grow flex items-center justify-center text-[18px] leading-[32px] font-[400px] text-[#31323E]">
-                Our data analytics service provides you with actionable insights that help you make informed decisions, optimize operations, and drive business growth.
+                {content}
             </div>
 
             {/* Third div - Bottom */}
             <div className="flex-shrink-0">
-                <ButtonSection />
+                <ButtonSection
+                    button1Name={button1Name}
+                    button2Name={button2Name}
+                    button3Name={button3Name}
+                    button4Name={button4Name}
+                    button5Name={button5Name}
+
+                />
             </div>
         </div>
     );
